37bf4ba172c705ab97d0510658a62e3e4274e984
Techharik <khariprasath30@gmail.com>
2026-04-01 19:22:37 +0530

    feat: base setup

--- apps/backend/src/shared/config/config.ts
+++ apps/backend/src/shared/config/config.ts
@@ -1,8 +1,12 @@
 type configTypes = {
-  PORT: number;
+  PORT: string;
+  DATABASE_URL: string;
+  NODE_ENV: string;
 };
 
 const config: configTypes = {
-  PORT: 3000,
+  PORT: process.env.PORT || "3000",
+  DATABASE_URL: process.env.DATABASE_URL || "",
+  NODE_ENV: process.env.NODE_ENV || "development",
 };
 export default config;
