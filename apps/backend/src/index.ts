ef79feef111e256b8e800a6ae58d97acdd058e85
hariprasath <khariprasath30@gmail.com>
2026-04-01 12:10:34 +0530

    feat: express setup

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,1 +1,14 @@
-import "dotenv/config"; // Must be the first line
+import "dotenv/config";
+import express, { type Request, type Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello world");
+});
+
+app.listen(3000, (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
